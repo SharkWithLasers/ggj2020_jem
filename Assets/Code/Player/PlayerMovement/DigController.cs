5253a84749cfd666e6d54963c7010038de311179
Joel Milligan <joel.milligan@merrillcorp.com>
2020-02-01 17:01:11 -0500

    Joel: Add buffs scripts

--- Assets/Code/Player/PlayerMovement/DigController.cs
+++ Assets/Code/Player/PlayerMovement/DigController.cs
@@ -39,4 +39,14 @@ public class DigController : MonoBehaviour
     {
         overlappingGrave = Option<Grave>.None;
     }
+
+    public void IncreaseDigSpeed(float modifier)
+    {
+      digSpeed += modifier;
+    }
+
+    public void DecreaseDigSpeed(float modifier)
+    {
+      digSpeed -= modifier;
+    }
 }
