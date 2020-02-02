210dcc67627c78aae903e4e1ac239828cfc6350a
Joel Milligan <joel.milligan@merrillcorp.com>
2020-02-01 23:35:13 -0500

    Joel: Add some changes to nodes and animations

--- Assets/Code/BodyParts/LimbNode.cs
+++ Assets/Code/BodyParts/LimbNode.cs
@@ -6,6 +6,14 @@ public class LimbNode : MonoBehaviour
 {
   public LimbNodeType nodeType;
   public LimbNodeType attachedTo = LimbNodeType.NONE;
+  private Vector2 offsetFromCenter;
+
+  private void Start() {
+    float xOffset = transform.localPosition.x;
+    float yOffset = transform.localPosition.y;
+
+    offsetFromCenter = new Vector2(xOffset, yOffset);
+  }
 
   public void Attach(LimbNodeType nodeType)
   {
