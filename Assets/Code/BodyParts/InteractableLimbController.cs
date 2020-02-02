ed09ea3819afb767fc1984c4a8cc1175622960db
Michael Kamm <michaeljkamm@gmail.com>
2020-02-02 14:48:26 -0500

    hover

--- Assets/Code/BodyParts/InteractableLimbController.cs
+++ Assets/Code/BodyParts/InteractableLimbController.cs
@@ -1,40 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using DG.Tweening;
 public class InteractableLimbController : MonoBehaviour
 {
   InteractButtonController button;
+    private bool _canBeFuckedWith;
 
-  private void Start() {
+    private void Start() {
     button = gameObject.GetComponentInChildren<InteractButtonController>();
+        _canBeFuckedWith = true;
   }
 
-  private void OnTriggerStay2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        CheckForInput(other);
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
   {
     CheckForInput(other);
   }
 
   private void OnTriggerExit2D(Collider2D other) {
-    if (other.name == "PlayerInteraction")
-    {
-      button.Hide();
-    }
+
   }
 
   private void CheckForInput(Collider2D other)
   {
-    if (other.name == "PlayerInteraction")
-    {
-      button.Show();
-    }
 
-    if (Input.GetKey(KeyCode.E) && other.name == "PlayerInteraction")
+    if (_canBeFuckedWith && other.name == "PlayerInteraction")
     {
-      LimbNodeController nodeController = gameObject.GetComponent<LimbNodeController>();
-      other.GetComponentInChildren<BodyPartController>().AddBodyPartToList(nodeController.limbType);
+            _canBeFuckedWith = false;
+        LimbNodeController nodeController = gameObject.GetComponent<LimbNodeController>();
+        other.GetComponentInChildren<BodyPartController>().AddBodyPartToList(nodeController.limbType);
 
-      Destroy(gameObject);
+            //var    
+
+            //gameObject.transform.DOShakePosition(1);
+        gameObject.transform.DOScale(1.25f, 0.1f)
+                .SetLoops(10, LoopType.Yoyo)
+                .OnComplete(() => Destroy(gameObject));
     }
   }
 }
