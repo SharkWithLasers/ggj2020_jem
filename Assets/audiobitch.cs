ed0264a6b5919d6c9048ab8f666682e768c3390b
Joel Milligan <joel.milligan@merrillcorp.com>
2020-02-02 15:01:10 -0500

    MUSIC

---
+++ Assets/audiobitch.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audiobitch : MonoBehaviour
+{
+    public AudioClip intro;
+    public AudioClip loop;
+    private AudioSource source;
+
+    private void Start() {
+      source = gameObject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (source.isPlaying == false)
+      {
+        source.clip = loop;
+        source.Play();
+      }
+    }
+}
