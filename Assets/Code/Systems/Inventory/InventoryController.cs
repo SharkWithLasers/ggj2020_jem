0db91cbf25eee57aa5a9b4f90b40f8706c9af5d5
Michael Kamm <michaeljkamm@gmail.com>
2020-02-02 12:27:48 -0500

    UI complete

--- Assets/Code/Systems/Inventory/InventoryController.cs
+++ Assets/Code/Systems/Inventory/InventoryController.cs
@@ -5,14 +5,24 @@ using UnityEngine.UIElements;
 
 public class InventoryController : MonoBehaviour
 {
+    [SerializeField] private InventoryUIGrayout[] handGrayouts;
+
+    [SerializeField] private InventoryUIGrayout[] limbGrayouts;
+
+    [SerializeField] private InventoryUIGrayout[] feetGrayouts;
+
+    [SerializeField] private InventoryUIGrayout torsoGrayout;
+
+    [SerializeField] private InventoryUIGrayout headGrayout;
+
+    [SerializeField] private PlayerInventory playerInventory;
 
 
-  public GameObject handPrefab;
-  public GameObject limbPrefab;
-  public GameObject footPrefab;
+
 
   public void Add(LimbNodeType type)
   {
+        /*
     if (type == LimbNodeType.HAND) {
       Instantiate(handPrefab, transform);
     }
@@ -21,11 +31,55 @@ public class InventoryController : MonoBehaviour
     }
     else if (type == LimbNodeType.FOOT) {
       Instantiate(footPrefab, transform);
-    }
+    }*/
   }
 
     public void OnInventoryChanged()
     {
+        if (playerInventory.bodyPartToCount.ContainsKey(LimbNodeType.HAND))
+        {
+            var numHands = Mathf.Min(
+                playerInventory.bodyPartToCount[LimbNodeType.HAND],
+                handGrayouts.Length
+                );
+            for (int i = 0; i < numHands; i++)
+            {
+                handGrayouts[i].AddAsInventory();
+            }
+        }
 
+        if (playerInventory.bodyPartToCount.ContainsKey(LimbNodeType.FOOT))
+        {
+            var numFeet = Mathf.Min(
+                playerInventory.bodyPartToCount[LimbNodeType.FOOT],
+                feetGrayouts.Length);
+            for (int i = 0; i < numFeet; i++)
+            {
+                feetGrayouts[i].AddAsInventory();
+            }
+        }
+
+        if (playerInventory.bodyPartToCount.ContainsKey(LimbNodeType.TORSO))
+        {
+            torsoGrayout.AddAsInventory();
+        }
+
+        if (playerInventory.bodyPartToCount.ContainsKey(LimbNodeType.HEAD))
+        {
+            headGrayout.AddAsInventory();
+        }
+
+        if (playerInventory.bodyPartToCount.ContainsKey(LimbNodeType.LIMB))
+        {
+            var numLimbs = Mathf.Min(
+                playerInventory.bodyPartToCount[LimbNodeType.LIMB],
+                limbGrayouts.Length);
+
+            for (int i = 0; i < numLimbs*2; i+=2)
+            {
+                limbGrayouts[i].AddAsInventory();
+                limbGrayouts[i+1].AddAsInventory();
+            }
+        }
     }
 }
---
+++ Assets/Code/Systems/Inventory/InventoryUIGrayout.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryUIGrayout : MonoBehaviour
+{
+    private Image _spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _spriteRenderer = GetComponent<Image>();
+
+        _spriteRenderer.color = new Color(0.5f, 0.5f, 0.5f, 0.5f);
+    }
+
+
+    public void AddAsInventory()
+    {
+        _spriteRenderer.color = new Color(1f, 1f, 1f, 1f);
+
+    }
+}
